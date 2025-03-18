10982fbd37277a8c9ba0a9368a912581ff6c98d1
kindping <kindpingwork@gmail.com>
2025-03-18 20:01:12 +0800

    about-info 區塊API化

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,15 @@
 import {} from './main.js';
 import { load as loadAboutBanner } from './components/AboutBanner.js';
+import { AboutInfoAPI } from './api/AboutInfoAPI.js';
 
 loadAboutBanner('#about-banner-block');
+
+const aboutInfo = await AboutInfoAPI.get();
+let html = '';
+aboutInfo.forEach(function (info) {
+    html += `<${info.title_tag}>${info.title}</${info.title_tag}>
+    <div class="info-block">${info.content}</div>`;
+});
+
+let app = document.querySelector('#about-info-app');
+app.innerHTML = html;
