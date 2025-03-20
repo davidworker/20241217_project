a3829ac66ab4b7c17c801739ac060228bda79dc1
kindping <kindpingwork@gmail.com>
2025-03-20 19:35:43 +0800

    partner-company 元件化

--- assets/js/partner.js
+++ assets/js/partner.js
@@ -1,4 +1,6 @@
 import {} from './main.js';
 import { load as PageBanner } from './components/PageBanner.js';
+import { load as PartnerCompany } from './components/PartnerCompany.js';
 
 PageBanner('#partner-banner-block', '合作夥伴');
+PartnerCompany('#partner-company-block');
