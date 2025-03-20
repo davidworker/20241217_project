e12d33d487cfa9cb1845a532daf7a7919081c2c1
kindping <kindpingwork@gmail.com>
2025-03-20 21:09:07 +0800

    contact-form 元件化

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,6 @@
 import {} from './main.js';
 import { load as PageBanner } from './components/PageBanner.js';
+import { load as ContactForm } from './components/ContactForm.js';
 
 PageBanner('#contact-banner-block', '聯絡我們');
+ContactForm('#contact-form-block');
