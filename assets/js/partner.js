223951b026de7fda49673a8b7b8509c31ac359ab
kindping <kindpingwork@gmail.com>
2025-03-20 20:17:47 +0800

    partner-contact-block 引用通用元件 page-contact

--- assets/js/partner.js
+++ assets/js/partner.js
@@ -2,7 +2,14 @@ import {} from './main.js';
 import { load as PageBanner } from './components/PageBanner.js';
 import { load as PartnerCompany } from './components/PartnerCompany.js';
 import { load as PartnerIndustry } from './components/PartnerIndustry.js';
+import { load as PageContact } from './components/PageContact.js';
 
 PageBanner('#partner-banner-block', '合作夥伴');
 PartnerCompany('#partner-company-block');
 PartnerIndustry('#partner-industry-block');
+PageContact(
+    '#partner-contact-block',
+    '成為合作夥伴',
+    '我們歡迎各產業的合作提案，一起創造更多可能性',
+    '聯絡我們'
+);
