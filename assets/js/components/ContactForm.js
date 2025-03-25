d6fac81fd15017439100804b40cb0119255f6028
kindping <kindpingwork@gmail.com>
2025-03-25 19:08:21 +0800

    新增聯絡表單功能，整合 Vue.js 並更新表單提交邏輯

--- assets/js/components/ContactForm.js
+++ assets/js/components/ContactForm.js
@@ -1,11 +1,36 @@
 import { loadFile } from './Utils.js';
 
+const initApp = function () {
+    const { createApp } = Vue;
+    const options = {
+        data() {
+            return {
+                name: '',
+                email: '',
+                subject: '',
+                message: '',
+            };
+        },
+        methods: {
+            doSubmit() {
+                console.log('do submit');
+            },
+        },
+        mounted() {
+            console.log('contact form mounted');
+        },
+    };
+    const app = createApp(options);
+    app.mount('#contact-form-app');
+};
+
 /** 載入聯絡表單
  * @param {string} selector 載入的元素
  * @returns {void}
  */
 const load = async function (selector) {
     await loadFile(selector, './components/contact-form.html');
+    initApp();
 };
 
 export { load };
