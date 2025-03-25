ae3e16d6ab90760f39277b562fff654122a9653c
kindping <kindpingwork@gmail.com>
2025-03-25 19:24:12 +0800

    contact-form 串接 google apps script

--- assets/js/components/ContactForm.js
+++ assets/js/components/ContactForm.js
@@ -1,19 +1,56 @@
 import { loadFile } from './Utils.js';
 
+const APP_URL =
+    'https://script.google.com/macros/s/AKfycbxuUbEdd0mypejefT69813hfItGGhuQ-zYqme8s3Oaq0U7KJdvzQSNwlYvMTEYco6XVxw/exec';
+
 const initApp = function () {
     const { createApp } = Vue;
     const options = {
         data() {
             return {
-                name: '',
-                email: '',
-                subject: '',
-                message: '',
+                db: {
+                    name: '',
+                    email: '',
+                    subject: '',
+                    message: '',
+                },
+                url: APP_URL,
             };
         },
         methods: {
-            doSubmit() {
-                console.log('do submit');
+            async doSubmit() {
+                let formData = new FormData();
+
+                const requires = {
+                    name: '請輸入姓名',
+                    email: '請輸入電子郵件',
+                    subject: '請輸入主旨',
+                    message: '請輸入訊息內容',
+                };
+
+                for (const key in requires) {
+                    if (this.db[key] === '') {
+                        alert(requires[key]);
+                        return;
+                    }
+                    formData.append(key, this.db[key]);
+                }
+
+                try {
+                    let response = await fetch(this.url, {
+                        method: 'POST',
+                        body: formData,
+                    });
+
+                    if (response.ok) {
+                        alert('訊息已送出');
+                        return;
+                    }
+
+                    alert('訊息送出失敗');
+                } catch (e) {
+                    console.log(e);
+                }
             },
         },
         mounted() {
