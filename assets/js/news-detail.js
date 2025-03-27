f111b3779fff6947d988b774f72e692e55278146
kindping <kindpingwork@gmail.com>
2025-03-27 19:44:55 +0800

    news-detail-body 元件

--- assets/js/news-detail.js
+++ assets/js/news-detail.js
@@ -1,6 +1,9 @@
 import {} from './main.js';
 import { load as PageBanner } from './components/PageBanner.js';
+import { load as NewsDetailBody } from './components/NewsDetailBody.js';
 
 PageBanner('#news-detail-banner-block', '最新消息', {
     subtitle: '掌握老夫科技最新動態，了解產業創新趨勢',
 });
+
+NewsDetailBody('#news-detail-body-block');
